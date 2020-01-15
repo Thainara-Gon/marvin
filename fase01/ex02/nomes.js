ccaf005a505339ed2edfc0c2f52d38a66a649d99
Thainara-Gon <59850798+Thainara-Gon@users.noreply.github.com>
2020-01-15 19:57:33 -0300

    3

--- fase01/ex02/nomes.js
+++ fase01/ex02/nomes.js
@@ -1,22 +1,4 @@
-function mostrarNome() {
-  
+function nomes (nome1, nome2){
+    console.log(nomes("Arthur", "Zaphod"));    
 }
 
-console.log ("Marvin");
-
-function nomeDeVolta() {
-    
-}
-
-console.log ('O andróde paranóide');
-
-function nomes(nome1, nome2) {
-    
-}
-console.log ("Arthur e Zaphod")
-
-function nomes(nome3, nome4){
-    
-}
-
-console.log("Marvin e Cachalote")
