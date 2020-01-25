5a422facaa5f81e637ecebf230fa099cb0e9f2cf
Thainara-Gon <59850798+Thainara-Gon@users.noreply.github.com>
2020-01-24 21:20:32 -0300

    2

--- fase03/ex02/pares.js
+++ fase03/ex02/pares.js
@@ -1,11 +1,12 @@
-function pares(num1){
-     if (num1%2 ===0)
-    {
+function pares(num1,num2){
+     if (num1%2 ===0 && num2%2 ===0)
+    
         return("Os números são pares");
         
-    }else{
+    else
         return("Os números não são pares");
-    }
+    
+    
     
 }
 
