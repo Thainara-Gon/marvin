1fbb616f6eda4a77e57e898eed165ea6024b64c0
Thainara-Gon <59850798+Thainara-Gon@users.noreply.github.com>
2020-01-27 19:39:09 -0300

    5

---
+++ fase03/ex05/acharNumero.js
@@ -0,0 +1,11 @@
+function acharNumero(num){
+  if (num < 5)
+    return ("mercurio");
+ else if (num <= 10 && num >=5)
+     return ("venus");
+ else if (num >10 && num<= 100 )
+     return( "terra");
+ else 
+  return ("marte")
+}
+
