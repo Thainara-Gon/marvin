d25ccda8e067efb3a6df115ff7ae864f82e97150
Thainara-Gon <59850798+Thainara-Gon@users.noreply.github.com>
2020-01-29 19:43:26 -0300

    1

--- fase03/ex01/nove.js
+++ fase03/ex01/nove.js
@@ -1,8 +1,9 @@
-function nove(num){
-    if ( num.substr(-1) ==9){
-        return ("true");
-    }else{
-        return ("false");
-    }
+function nove(numero)   {
+    var n1 = numero.toString();
+    var n2 = n1.substring(n1.length-1,n1.length);
+    
+    if (n2==="9")
+    return true
+    else 
+    return false
 }
-
